21c990b06da2ffe11c3c17e9bc097501a12cb221
Emerick Poulin <emerick.poulin@omnirobotic.com>
2022-02-16 22:34:14 -0500

    use category id

--- frontend/src/components/CardMasonry.tsx
+++ frontend/src/components/CardMasonry.tsx
@@ -1,9 +1,5 @@
 import React from "react";
 import Masonry from "@mui/lab/Masonry";
-import {Card, CardActions, CardContent, CardMedia, IconButton, Typography} from "@mui/material";
-import UserHistory from "../icons/UserHistoryIcon";
-import ClearIcon from "@mui/icons-material/Clear";
-import NotListedLocationIcon from '@mui/icons-material/NotListedLocation';
 import {ItemCard} from "./ItemCard";
 import {CategoryCard} from "./CategoryCard";
 
@@ -19,7 +15,7 @@ export const CardMasonry = ({numbers}: CardMasonryParams) => {
                 if (i % 2 == 0) {
                     return <ItemCard id={i} itemText={i + "text"} itemTitle="Title"/>
                 } else {
-                    return <CategoryCard categoryName={"Category"}/>
+                    return <CategoryCard id={i} categoryName={"Category"}/>
                 }
             })}
         </Masonry>
