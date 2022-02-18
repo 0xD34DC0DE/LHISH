b2fc6d839432d3d66674fb9d883785cd188cee1c
Emerick Poulin <emerick.poulin@omnirobotic.com>
2022-02-18 00:49:15 -0500

    start implementing protected route wrapper

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,23 +1,32 @@
-import React, {useRef} from 'react';
+import React, {useContext} from 'react';
 import './App.css';
-import {SessionContextProvider} from "./contexts/SessionContext";
-import {AppBar, Avatar, Box, Container, IconButton, Toolbar,} from "@mui/material";
-import MenuIcon from '@mui/icons-material/Menu';
-import {SearchBar} from "./components/SearchBar";
-import {DrawerRef, MenuDrawer} from "./components/MenuDrawer";
-import {DrawerMenuItem} from "./components/DrawerMenuItem";
-import {Route, Routes, useNavigate} from "react-router-dom";
-import FavoritesPage from "./pages/FavoritesPage";
-
-// const Item = styled(Paper)(({theme}) => ({
-//     backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
-//     ...theme.typography.body2,
-//     padding: theme.spacing(0.5),
-//     textAlign: 'center',
-//     color: theme.palette.text.secondary,
-// }));
+import SessionContext, {SessionContextProvider} from "./contexts/SessionContext";
 
 
+interface ProtectedRouteProps {
+    requiresLoggedIn: boolean, // Find a way to make it a prop that doesn't require a value -> like the disabled prop
+    path: string,
+    role: string | string []
+    element: React.ReactNode
+}
+
+const ProtectedRoute = ({path}: ProtectedRouteProps) => {
+/*
+* TODO use <Routes>
+* then filter children of <Routes>
+* (instances of <Route> using ProtectedRouteProps) based on context role and login state)
+*/
+}
+
+const ProtectedRoutes: React.FunctionComponent = ({children}) => {
+    const {state, dispatch} = useContext(SessionContext);
+
+    return (
+        <>
+
+        </>
+    );
+}
 
 function App() {
     return (
