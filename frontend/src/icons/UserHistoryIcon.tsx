6dfa4dc6011dd55b435bcffc341fa3d3aa5fcb20
Emerick Poulin <emerick.poulin@omnirobotic.com>
2022-02-17 19:33:48 -0500

    fix warnings

--- frontend/src/icons/UserHistoryIcon.tsx
+++ frontend/src/icons/UserHistoryIcon.tsx
@@ -4,8 +4,8 @@ import {SvgIcon} from "@mui/material";
 const UserHistoryIcon = () => {
     return (
         <SvgIcon viewBox="240 241 16.51 16">
-            <path xmlns="http://www.w3.org/2000/svg" vector-effect="non-scaling-stroke" d="M252.81 249.46a3.69 3.69 0 0 0-3.69 3.69h-1.23l1.6 1.6.03.05 1.65-1.65h-1.22a2.87 2.87 0 1 1 .84 2.03l-.58.58a3.69 3.69 0 1 0 2.6-6.3zm-.4 2.05v2.05l1.75 1.04.3-.5-1.44-.85v-1.74zM244 245a4 4 0 1 1 8 0 4 4 0 0 1-8 0z"/>
-            <path xmlns="http://www.w3.org/2000/svg" vector-effect="non-scaling-stroke" d="M248.35 251c-2.73-.1-8.35 1.27-8.35 4v2h9.54a5.95 5.95 0 0 1-1.19-6z"/>
+            <path xmlns="http://www.w3.org/2000/svg" d="M252.81 249.46a3.69 3.69 0 0 0-3.69 3.69h-1.23l1.6 1.6.03.05 1.65-1.65h-1.22a2.87 2.87 0 1 1 .84 2.03l-.58.58a3.69 3.69 0 1 0 2.6-6.3zm-.4 2.05v2.05l1.75 1.04.3-.5-1.44-.85v-1.74zM244 245a4 4 0 1 1 8 0 4 4 0 0 1-8 0z"/>
+            <path xmlns="http://www.w3.org/2000/svg" d="M248.35 251c-2.73-.1-8.35 1.27-8.35 4v2h9.54a5.95 5.95 0 0 1-1.19-6z"/>
         </SvgIcon>
     )
 }
