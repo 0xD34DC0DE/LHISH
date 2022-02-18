a0d78d7ebd6bd7a493abf66dc532137515a82645
Emerick Poulin <emerick.poulin@omnirobotic.com>
2022-02-17 19:33:28 -0500

    add drawer menu links

--- frontend/src/components/DrawerMenuItem.tsx
+++ frontend/src/components/DrawerMenuItem.tsx
@@ -1,22 +1,22 @@
-import {ListItem, ListItemIcon, ListItemText} from "@mui/material";
+import {Link, ListItem, ListItemIcon, ListItemText} from "@mui/material";
 import CategoryIcon from '@mui/icons-material/Category';
 import ListAltIcon from '@mui/icons-material/ListAlt';
-
-export interface IDrawerMenuItem {
-}
 
 export interface DrawerMenuItemPropTypes {
     key?: number,
     menuName: string,
+    onClick: () => void
 }
 
-export const DrawerMenuItem: IDrawerMenuItem = ({key = 0, menuName}: DrawerMenuItemPropTypes) => {
+export const DrawerMenuItem = ({key = 0, menuName, onClick}: DrawerMenuItemPropTypes) => {
     return (
-        <ListItem button key={key}>
-            <ListItemIcon>
-                {key % 2 === 0 ? <CategoryIcon/> : <ListAltIcon/>}
-            </ListItemIcon>
-            <ListItemText primary={menuName}/>
-        </ListItem>
+        <Link key={key} href="#" underline={"none"} color={"inherit"} onClick={onClick}>
+            <ListItem button key={key}>
+                <ListItemIcon>
+                    {key % 2 === 0 ? <CategoryIcon/> : <ListAltIcon/>}
+                </ListItemIcon>
+                <ListItemText primary={menuName}/>
+            </ListItem>
+        </Link>
     );
 };
