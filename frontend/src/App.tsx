e354a0a0255d72083a0d364af1e28c6b1a82a02f
Emerick Poulin <emerick.poulin@omnirobotic.com>
2022-02-18 00:48:48 -0500

    refactor FavoritesPage into its own component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -20,66 +20,11 @@ import FavoritesPage from "./pages/FavoritesPage";
 
 
 function App() {
-    const drawerRef = useRef<DrawerRef>(null);
-    const navigate = useNavigate();
-
-    const openDrawer = () => {
-        if (drawerRef.current) {
-            drawerRef.current.openDrawer()
-        }
-    }
-
-    const closeDrawer = () => {
-        if (drawerRef.current) {
-            drawerRef.current.closeDrawer()
-        }
-    }
-
-    const navigate_to = (path: string) => {
-        closeDrawer();
-        navigate(path, {replace: true});
-    }
-
     return (
         <SessionContextProvider>
-            <AppBar position="sticky">
-                <MenuDrawer ref={drawerRef}>
-                    <DrawerMenuItem menuName={"Favorites"} onClick={() => navigate_to("/favorites")}/>
-                    <DrawerMenuItem menuName={"Categories"} onClick={() => navigate_to("/")}/>
-                </MenuDrawer>
-                <Box>
-                    <Toolbar variant="dense" sx={{
-                        display: "flex",
-                        flexDirection: "row",
-                        justifyContent: "space-between",
-                        alignItems: "center"
-                    }}>
+            <ProtectedRoutes>
 
-                        <IconButton onClick={openDrawer} edge="start" color="inherit" aria-label="menu"
-                                    sx={{mr: 2}}>
-                            <MenuIcon/>
-                        </IconButton>
-
-                        <Box sx={{flexGrow: 0.6}}>
-                            <SearchBar label={"Quick Search..."} onSubmit={() => {
-                            }}/>
-                        </Box>
-
-                        <IconButton>
-                            <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg"/>
-                        </IconButton>
-
-                    </Toolbar>
-                </Box>
-            </AppBar>
-
-            <Container>
-                <Routes>
-                    <Route path="/favorites" element={<FavoritesPage/>}/>
-                    <Route path="/" element={<FavoritesPage/>}/>
-                </Routes>
-            </Container>
-
+            </ProtectedRoutes>
         </SessionContextProvider>
     );
 }
