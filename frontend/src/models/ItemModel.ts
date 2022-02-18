1d99ca8f9923f8d601fa08dd760176da0c779c2b
Emerick Poulin <emerick.poulin@omnirobotic.com>
2022-02-18 00:51:17 -0500

    add card models

---
+++ frontend/src/models/ItemModel.ts
@@ -0,0 +1,14 @@
+
+export enum Availability {
+    Available,
+    Taken,
+    Empty,
+    Unknown
+}
+
+export default interface IItem {
+    id: string,
+    name: string,
+    description: string, //TODO replace by ReactNodes made by the factory/renderer
+    availability: Availability,
+}
