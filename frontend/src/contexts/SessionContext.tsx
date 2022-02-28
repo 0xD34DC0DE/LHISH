b89b9eaa554ea9f6d8e78ecaa69b284296e1fba4
Emerick Poulin <emerick.poulin@omnirobotic.com>
2022-02-27 21:41:14 -0500

    refactor and add loggin fields

--- frontend/src/contexts/SessionContext.tsx
+++ frontend/src/contexts/SessionContext.tsx
@@ -1,42 +1,44 @@
 import React, {createContext, ReactChild, ReactChildren, useReducer} from "react";
 
 interface SessionContextState {
-    username: string,
     user_id: string,
+    username: string,
+    role: string;
     token: string,
     loggedIn: boolean,
 }
 
 const initialState: SessionContextState = {
-    username: "",
     user_id: "",
+    username: "",
+    role: "",
     token: "",
     loggedIn: false,
 }
 
 type Action =
-    | { type: "login" } // Can have objects after like the new state -> { type: 'login', new state}
+    | { type: "login", userId: string, password: string } // Can have objects after like the new state -> { type: 'login', new state}
     | { type: "logout" }
     | { type: "refresh" };
 
-function SessionContextReducer(state: SessionContextState, action: Action): SessionContextState {
+function SessionContextReducer(session: SessionContextState, action: Action): SessionContextState {
     switch (action.type) {
         case "login":
-            return {...state, loggedIn: true}
+            return {...session, loggedIn: true, role: "user"}
         case "logout":
-            return {...state, loggedIn: false}
+            return {...session, loggedIn: false}
         case "refresh":
-            return state
+            return session
     }
 }
 
 type SessionContextProviderType = {
-    state: SessionContextState,
+    session: SessionContextState,
     dispatch: React.Dispatch<Action>
 }
 
 const SessionContext = createContext<SessionContextProviderType>({
-    state: initialState,
+    session: initialState,
     dispatch: () => null
 });
 
@@ -47,11 +49,11 @@ interface AuxProps {
 }
 
 export const SessionContextProvider = ({children}: AuxProps) => {
-    const [state, dispatch] = useReducer(SessionContextReducer, {...initialState});
+    const [session, dispatch] = useReducer(SessionContextReducer, {...initialState});
 
 
     return (
-        <SessionContext.Provider value={{state, dispatch}}>
+        <SessionContext.Provider value={{session: session, dispatch}}>
             {children}
         </SessionContext.Provider>
     );
