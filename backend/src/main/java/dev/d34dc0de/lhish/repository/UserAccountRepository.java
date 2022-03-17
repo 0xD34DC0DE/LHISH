db94e67c9831ee2d2a28c47b9b048cb29b643138
Emerick Poulin <emerick.poulin@omnirobotic.com>
2022-03-17 18:12:14 -0400

    add user account repository

--- backend/src/main/java/dev/d34dc0de/lhish/repository/UserAccountRepository.java
+++ backend/src/main/java/dev/d34dc0de/lhish/repository/UserAccountRepository.java
@@ -1,14 +1,13 @@
 package dev.d34dc0de.lhish.repository;
 
-import dev.d34dc0de.lhish.model.Account;
+import dev.d34dc0de.lhish.model.UserAccount;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
-public interface UserAccountRepository extends MongoRepository<Account, String> {
-    Optional<Account> findByUsernameAndPassword(String username, String password);
+public interface UserAccountRepository extends MongoRepository<UserAccount, String> {
 
-    Optional<Account> findByUsername(String username);
+    Optional<UserAccount> findByUsername(String username);
 }
