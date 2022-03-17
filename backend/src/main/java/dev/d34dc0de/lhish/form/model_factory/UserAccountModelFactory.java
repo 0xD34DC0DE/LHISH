ac26b7d5ea87b59dfb6a44bfd004fdcd48aa283e
Emerick Poulin <emerick.poulin@omnirobotic.com>
2022-03-17 18:10:52 -0400

    implement user account factory

--- backend/src/main/java/dev/d34dc0de/lhish/form/model_factory/UserAccountModelFactory.java
+++ backend/src/main/java/dev/d34dc0de/lhish/form/model_factory/UserAccountModelFactory.java
@@ -1,4 +1,21 @@
-package dev.d34dc0de.lhish.form;
+package dev.d34dc0de.lhish.form.model_factory;
 
-public class model_factory {
+import dev.d34dc0de.lhish.form.UserAccountCreationForm;
+import dev.d34dc0de.lhish.model.UserAccount;
+import dev.d34dc0de.lhish.model.enums.Role;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+@Component
+public abstract class UserAccountModelFactory {
+
+    public static UserAccount toModel(PasswordEncoder passwordEncoder, UserAccountCreationForm accountCreationForm) {
+        return UserAccount.userAccountBuilder()
+                .username(accountCreationForm.username())
+                .password(passwordEncoder.encode(accountCreationForm.password()))
+                .email(accountCreationForm.email())
+                .role(Role.USER)
+                .build();
+    }
+
 }
