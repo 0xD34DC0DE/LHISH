76c1ef2305bca6f73bfad686bbd5b7a7d5255e3c
Emerick Poulin <emerick.poulin@omnirobotic.com>
2022-03-17 18:11:55 -0400

    add jwt account model

--- backend/src/main/java/dev/d34dc0de/lhish/model/JwtAccount.java
+++ backend/src/main/java/dev/d34dc0de/lhish/model/JwtAccount.java
@@ -1,4 +1,18 @@
 package dev.d34dc0de.lhish.model;
 
-public interface JwtAuthenticated {
+import dev.d34dc0de.lhish.model.enums.Role;
+
+public interface JwtAccount {
+
+    String getId();
+    void setId(String id);
+
+    String getUsername();
+    void setUsername(String username);
+
+    String getEmail();
+    void setEmail(String email);
+
+    Role getRole();
+    void setRole(Role role);
 }
