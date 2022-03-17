5ed5eb896ba1a8b312ec9161171b9430634c503b
Emerick Poulin <emerick.poulin@omnirobotic.com>
2022-03-17 18:11:39 -0400

    implement account and user account models

--- backend/src/main/java/dev/d34dc0de/lhish/model/Account.java
+++ backend/src/main/java/dev/d34dc0de/lhish/model/Account.java
@@ -4,19 +4,20 @@ import dev.d34dc0de.lhish.model.enums.Role;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @AllArgsConstructor
+@NoArgsConstructor
 @Builder
 @Data
 @Document("accounts")
-public class Account {
-
+public class Account implements JwtAccount {
     @Id
     private String id;
-    Role role;
     private String username;
     private String email;
+    private Role role;
     private String password;
 }
--- backend/src/main/java/dev/d34dc0de/lhish/model/UserAccount.java
+++ backend/src/main/java/dev/d34dc0de/lhish/model/UserAccount.java
@@ -1,11 +1,15 @@
 package dev.d34dc0de.lhish.model;
 
 import dev.d34dc0de.lhish.model.enums.Role;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 
+@AllArgsConstructor
 public class UserAccount extends Account {
 
-    public UserAccount(String id, String username, String email, String password) {
-        super(id, Role.USER, username, email, password);
+    @Builder(builderMethodName = "userAccountBuilder")
+    public UserAccount(String id, String username, String email, Role role, String password) {
+        super(id, username, email, role, password);
     }
-
 }
+
