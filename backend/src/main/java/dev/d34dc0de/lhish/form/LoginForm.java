3a07c9049a315406eb59ca8bcc0c22c428d68b88
Emerick Poulin <emerick.poulin@omnirobotic.com>
2022-03-17 18:11:14 -0400

    implement login and registering forms

--- backend/src/main/java/dev/d34dc0de/lhish/form/LoginForm.java
+++ backend/src/main/java/dev/d34dc0de/lhish/form/LoginForm.java
@@ -1,2 +1,4 @@
-package dev.d34dc0de.lhish.form;public record LoginForm() {
+package dev.d34dc0de.lhish.form;
+
+public record LoginForm(String username, String password) {
 }
--- backend/src/main/java/dev/d34dc0de/lhish/form/UserAccountCreationForm.java
+++ backend/src/main/java/dev/d34dc0de/lhish/form/UserAccountCreationForm.java
@@ -1,2 +1,4 @@
-package dev.d34dc0de.lhish.form;public record UserAccountCreationForm() {
+package dev.d34dc0de.lhish.form;
+
+public record UserAccountCreationForm(String username, String email, String password) {
 }
