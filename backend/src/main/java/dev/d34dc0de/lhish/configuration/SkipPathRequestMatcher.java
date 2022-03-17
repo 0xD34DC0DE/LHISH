16cccdae8e0359b19ce1f5e3e08657f1f6241f25
Emerick Poulin <emerick.poulin@omnirobotic.com>
2022-03-17 18:09:28 -0400

    implement path matcher to skip certain paths

--- backend/src/main/java/dev/d34dc0de/lhish/configuration/SkipPathRequestMatcher.java
+++ backend/src/main/java/dev/d34dc0de/lhish/configuration/SkipPathRequestMatcher.java
@@ -1,2 +1,30 @@
-package dev.d34dc0de.lhish.configuration;public class SkipPathRequestMatcher {
+package dev.d34dc0de.lhish.configuration;
+
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.security.web.util.matcher.OrRequestMatcher;
+import org.springframework.security.web.util.matcher.RequestMatcher;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+public class SkipPathRequestMatcher implements RequestMatcher {
+    private final OrRequestMatcher matchers;
+    private final RequestMatcher processingMatcher;
+
+    public SkipPathRequestMatcher(List<String> pathsToSkip, String processingPath) {
+        Objects.requireNonNull(pathsToSkip);
+        List<RequestMatcher> m = pathsToSkip.stream().map(AntPathRequestMatcher::new).collect(Collectors.toList());
+        matchers = new OrRequestMatcher(m);
+        processingMatcher = new AntPathRequestMatcher(processingPath);
+    }
+
+    @Override
+    public boolean matches(HttpServletRequest request) {
+        if (matchers.matches(request)) {
+            return false;
+        }
+        return processingMatcher.matches(request);
+    }
 }
