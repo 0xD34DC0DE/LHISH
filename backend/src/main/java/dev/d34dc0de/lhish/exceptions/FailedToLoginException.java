72ce525860a6442b0673687f555f2b19f670ed90
Emerick Poulin <emerick.poulin@omnirobotic.com>
2022-03-17 18:10:26 -0400

    implement login related custom exceptions

--- backend/src/main/java/dev/d34dc0de/lhish/exceptions/FailedToLoginException.java
+++ backend/src/main/java/dev/d34dc0de/lhish/exceptions/FailedToLoginException.java
@@ -1,2 +1,11 @@
-package dev.d34dc0de.lhish.exceptions;public class FailedToLoginException {
+package dev.d34dc0de.lhish.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.UNAUTHORIZED)
+public class FailedToLoginException extends Exception{
+    public FailedToLoginException(String username) {
+        super("Could not login with username: " + username);
+    }
 }
--- backend/src/main/java/dev/d34dc0de/lhish/exceptions/JwtAuthenticationException.java
+++ backend/src/main/java/dev/d34dc0de/lhish/exceptions/JwtAuthenticationException.java
@@ -1,2 +1,9 @@
-package dev.d34dc0de.lhish.exceptions;public class JwtAuthenticationException {
+package dev.d34dc0de.lhish.exceptions;
+
+import org.springframework.security.core.AuthenticationException;
+
+public class JwtAuthenticationException extends AuthenticationException {
+    public JwtAuthenticationException(String s) {
+        super(s);
+    }
 }
