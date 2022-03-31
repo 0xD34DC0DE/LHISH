e1a406b4b0a1d05fb24b7fc382fc4afe89d70678
Emerick Poulin <emerick.poulin@omnirobotic.com>
2022-03-30 21:22:45 -0400

    add category model

---
+++ backend/src/main/java/dev/d34dc0de/lhish/model/Category.java
@@ -0,0 +1,23 @@
+package dev.d34dc0de.lhish.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.bson.types.Binary;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Document("categories")
+public class Category {
+    @Id
+    String id;
+    String userId;
+    String name;
+    String description;
+    Binary image;
+}
