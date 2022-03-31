415d8feaf876c35194eb80ae8fb81f9979574e47
Emerick Poulin <emerick.poulin@omnirobotic.com>
2022-03-30 21:21:09 -0400

    add custom exception handler

---
+++ backend/src/main/java/dev/d34dc0de/lhish/exceptions/handler/WebExceptionHandler.java
@@ -0,0 +1,24 @@
+package dev.d34dc0de.lhish.exceptions.handler;
+
+import dev.d34dc0de.lhish.exceptions.APIException;
+import dev.d34dc0de.lhish.response.APIResponse;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+public class WebExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(value = {APIException.class})
+    protected ResponseEntity<Object> handleConflict(RuntimeException ex, WebRequest request) {
+        return handleExceptionInternal(ex,
+                APIResponse.error(ex.getCause().getMessage()),
+                new HttpHeaders(),
+                HttpStatus.OK,
+                request);
+    }
+}
