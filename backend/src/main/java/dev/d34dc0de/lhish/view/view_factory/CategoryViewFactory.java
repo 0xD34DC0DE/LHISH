95bbdb1e8696e5e1e1351ad9b504075cbc90f78c
Emerick Poulin <emerick.poulin@omnirobotic.com>
2022-03-30 23:02:49 -0400

    formatting

--- backend/src/main/java/dev/d34dc0de/lhish/view/view_factory/CategoryViewFactory.java
+++ backend/src/main/java/dev/d34dc0de/lhish/view/view_factory/CategoryViewFactory.java
@@ -6,7 +6,7 @@ import dev.d34dc0de.lhish.view.CategoryView;
 import java.util.Base64;
 
 public abstract class CategoryViewFactory {
-    
+
     public static String encode(byte[] bytes) {
         return Base64.getEncoder().encodeToString(bytes);
     }
