4a6628343a0eb74092599dea24a330d446683f18
Emerick Poulin <emerick.poulin@omnirobotic.com>
2022-03-30 21:21:55 -0400

    add custom exceptions in relation to file upload

---
+++ backend/src/main/java/dev/d34dc0de/lhish/exceptions/FileSizeLimitException.java
@@ -0,0 +1,7 @@
+package dev.d34dc0de.lhish.exceptions;
+
+public class FileSizeLimitException extends APIException {
+    public FileSizeLimitException(String message) {
+        super(message);
+    }
+}
