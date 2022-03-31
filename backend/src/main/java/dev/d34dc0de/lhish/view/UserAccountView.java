5ae24dec85f0544086c2bb0b4c00ef166042120a
Emerick Poulin <emerick.poulin@omnirobotic.com>
2022-03-30 21:24:42 -0400

    update account model

--- backend/src/main/java/dev/d34dc0de/lhish/view/UserAccountView.java
+++ backend/src/main/java/dev/d34dc0de/lhish/view/UserAccountView.java
@@ -1,6 +1,9 @@
 package dev.d34dc0de.lhish.view;
 
 import dev.d34dc0de.lhish.model.enums.Role;
+import lombok.Builder;
 
 public record UserAccountView (String id, String email, String username, Role role){
+    @Builder
+    public UserAccountView {};
 }
