6f312fc0d83b969957b6642514d886aaaf66d12d
Emerick Poulin <emerick.poulin@omnirobotic.com>
2022-04-02 11:57:15 -0400

    optimise imports

--- backend/src/main/java/dev/d34dc0de/lhish/configuration/SecurityConfig.java
+++ backend/src/main/java/dev/d34dc0de/lhish/configuration/SecurityConfig.java
@@ -3,18 +3,11 @@ package dev.d34dc0de.lhish.configuration;
 import dev.d34dc0de.lhish.security.JwtAuthFilter;
 import dev.d34dc0de.lhish.security.JwtAuthenticationEntryPoint;
 import dev.d34dc0de.lhish.security.JwtAuthenticationProvider;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-
-import java.util.Arrays;
-import java.util.Collections;
 
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
