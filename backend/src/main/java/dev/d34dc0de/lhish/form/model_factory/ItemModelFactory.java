87bc9b0bbe3ea9890c2fc9d24b28e807498c142e
Emerick Poulin <emerick.poulin@omnirobotic.com>
2022-04-02 12:00:50 -0400

    remove exception from method signature

--- backend/src/main/java/dev/d34dc0de/lhish/form/model_factory/ItemModelFactory.java
+++ backend/src/main/java/dev/d34dc0de/lhish/form/model_factory/ItemModelFactory.java
@@ -1,23 +1,14 @@
 package dev.d34dc0de.lhish.form.model_factory;
 
-import dev.d34dc0de.lhish.exceptions.FileSizeLimitException;
 import dev.d34dc0de.lhish.exceptions.NullFieldException;
-import dev.d34dc0de.lhish.form.CategoryCreationForm;
 import dev.d34dc0de.lhish.form.ItemCreationForm;
-import dev.d34dc0de.lhish.model.Category;
 import dev.d34dc0de.lhish.model.Item;
-import dev.d34dc0de.lhish.util.ImageUtils;
-import org.bson.types.Binary;
-
-import java.io.IOException;
 
 public abstract class ItemModelFactory {
 
+    public static Item toModel(ItemCreationForm itemCreationForm, String userId, String imageId) {
 
-
-    public static Item toModel(ItemCreationForm itemCreationForm, String userId, String imageId) throws IOException {
-
-        if(imageId == null) {
+        if (imageId == null) {
             throw new NullFieldException("Image cannot be null");
         }
 
