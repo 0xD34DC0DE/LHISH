a9c4f5f3099453354378f8027db701aa0d5ae2a1
Emerick Poulin <emerick.poulin@omnirobotic.com>
2022-04-02 12:01:12 -0400

    update category model factory

--- backend/src/main/java/dev/d34dc0de/lhish/form/model_factory/CategoryModelFactory.java
+++ backend/src/main/java/dev/d34dc0de/lhish/form/model_factory/CategoryModelFactory.java
@@ -1,32 +1,22 @@
 package dev.d34dc0de.lhish.form.model_factory;
 
-import dev.d34dc0de.lhish.exceptions.FileSizeLimitException;
 import dev.d34dc0de.lhish.exceptions.NullFieldException;
 import dev.d34dc0de.lhish.form.CategoryCreationForm;
 import dev.d34dc0de.lhish.model.Category;
-import org.bson.types.Binary;
-
-import java.io.IOException;
 
 public abstract class CategoryModelFactory {
 
-    private static final long MAX_FILE_SIZE = 16_777_216; // 16 MB
+    public static Category toModel(CategoryCreationForm categoryCreationForm, String userId, String imageId) {
 
-    public static Category toModel(CategoryCreationForm categoryCreationForm, String userId) throws IOException {
-
-        if(categoryCreationForm.image() == null) {
+        if (imageId == null) {
             throw new NullFieldException("Image cannot be null");
-        }
-
-        if (categoryCreationForm.image().getBytes().length >= MAX_FILE_SIZE) {
-            throw new FileSizeLimitException("File size limit of 16MB exceeded");
         }
 
         return Category.builder()
                 .userId(userId)
                 .name(categoryCreationForm.name())
                 .description(categoryCreationForm.description())
-                .image(new Binary(categoryCreationForm.image().getBytes()))
+                .imageId(imageId)
                 .build();
     }
 
