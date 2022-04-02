899f28280927a525c436e854dce204d565e48031
Emerick Poulin <emerick.poulin@omnirobotic.com>
2022-04-02 14:28:12 -0400

    add category selection

--- frontend/src/components/CreateItemDialog.tsx
+++ frontend/src/components/CreateItemDialog.tsx
@@ -1,8 +1,21 @@
 import DialogBase, {DialogBaseRef} from "./DialogBase"
-import {Box, Button, DialogActions, DialogContent, DialogTitle, TextField, Typography} from "@mui/material";
+import {
+    Box,
+    Button,
+    DialogActions,
+    DialogContent,
+    DialogTitle,
+    FormControl,
+    InputLabel,
+    MenuItem,
+    Select,
+    SelectChangeEvent,
+    TextField,
+    Typography
+} from "@mui/material";
 import React, {useEffect, useState} from "react";
 import {FileUploadButton} from "./FileUploadButton";
-import {useAuthFormPost, useAuthGet, useAuthPost} from "../hooks/QueryHooks";
+import {useAuthFormPost, useAuthGet} from "../hooks/QueryHooks";
 import {green, red} from "@mui/material/colors";
 
 export interface CreateItemDialogProps {
@@ -13,14 +26,25 @@ export interface CreateItemDialogProps {
 export const CreateItemDialog = ({innerRef, onItemCreated}: CreateItemDialogProps) => {
     const [itemName, setItemName] = useState("Item name");
     const [description, setDescription] = useState("");
+    const [category, setCategory] = useState("");
     const [file, setFile] = useState<File>();
 
-    const [postForm, data, error, reset] = useAuthFormPost(
+    const [postForm, formData, formError] = useAuthFormPost(
         "http://localhost:8080/item/create"
     );
 
+    const [getCategoryIds, categoryIds, categoryIdsError] = useAuthGet<string[]>(
+        "http://localhost:8080/category/all/ids"
+    );
+
+    useEffect(() => {
+        if (!categoryIds) {
+            getCategoryIds();
+        }
+    }, []);
+
     const onSubmit = () => {
-        if(!file) return;
+        if (!file) return;
         postForm(
             ["name", itemName],
             ["description", description],
@@ -52,17 +76,32 @@ export const CreateItemDialog = ({innerRef, onItemCreated}: CreateItemDialogProp
                     rows={4}
                     onChange={(e: React.ChangeEvent<HTMLInputElement>) => setDescription(e.target.value)}
                 />
-
+                <FormControl fullWidth>
+                    <InputLabel id="category-id-select-label">Category</InputLabel>
+                    <Select
+                        labelId="category-id-select-label"
+                        id="category-id-select"
+                        value={category}
+                        label="Category"
+                        onChange={(e: SelectChangeEvent) => setCategory(e.target.value)}
+                    >
+                        {categoryIds && categoryIds.map(id => (
+                            <MenuItem key={id} value={id}>{id}</MenuItem>
+                        ))}
+                    </Select>
+                </FormControl>
                 <FileUploadButton onFileChanged={setFile} accept={"image/*"} id={"image-upload"}/>
             </DialogContent>
 
             <DialogActions>
                 <Box sx={{marginLeft: 2, marginRight: "auto"}}>
-                    {data && <Typography sx={{color: green[500]}}>Item: {itemName} created</Typography>}
-                    {error && <Typography sx={{color: red[500]}}>Could not create item
-                        "{itemName}" <br/>Reason: {error}</Typography>}
+                    {formData && <Typography sx={{color: green[500]}}>Item: {itemName} created</Typography>}
+                    {formError && <Typography sx={{color: red[500]}}>Could not create item
+                        "{itemName}" <br/>Reason: {formError}</Typography>}
+                    {categoryIdsError && <Typography sx={{color: red[500]}}>Could not create item
+                        "{itemName}" <br/>Reason: {formError}</Typography>}
                 </Box>
-                <Button onClick={onSubmit}>Create</Button>
+                <Button disabled={categoryIdsError != null} onClick={onSubmit}>Create</Button>
             </DialogActions>
 
         </DialogBase>
