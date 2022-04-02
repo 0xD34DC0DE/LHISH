ae2d52a48db9844178bd885b1512d7316e032c1f
Emerick Poulin <emerick.poulin@omnirobotic.com>
2022-04-02 11:56:07 -0400

    update item view

--- backend/src/main/java/dev/d34dc0de/lhish/view/ItemView.java
+++ backend/src/main/java/dev/d34dc0de/lhish/view/ItemView.java
@@ -2,7 +2,7 @@ package dev.d34dc0de.lhish.view;
 
 import lombok.Builder;
 
-public record ItemView(String id, String name, String description, String image) {
+public record ItemView(String id, String name, String description, String imageId) {
     @Builder
     public ItemView {};
 }
