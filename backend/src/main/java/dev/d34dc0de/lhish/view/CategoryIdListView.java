9414cb656856a21b4142735328113bc22ff4de05
Emerick Poulin <emerick.poulin@omnirobotic.com>
2022-04-02 13:09:17 -0400

    add builder

--- backend/src/main/java/dev/d34dc0de/lhish/view/CategoryIdListView.java
+++ backend/src/main/java/dev/d34dc0de/lhish/view/CategoryIdListView.java
@@ -1,6 +1,10 @@
 package dev.d34dc0de.lhish.view;
 
+import lombok.Builder;
+
 import java.util.List;
 
 public record CategoryIdListView(List<String> categoryIds) {
+    @Builder
+    public CategoryIdListView{}
 }
