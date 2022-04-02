1a894659a29565e7e4febab64eb45c19331784b0
Emerick Poulin <emerick.poulin@omnirobotic.com>
2022-04-02 11:52:54 -0400

    add helper methods for api responses

--- backend/src/main/java/dev/d34dc0de/lhish/controller/BaseController.java
+++ backend/src/main/java/dev/d34dc0de/lhish/controller/BaseController.java
@@ -1,5 +1,8 @@
 package dev.d34dc0de.lhish.controller;
 
+import dev.d34dc0de.lhish.response.APIResponse;
+import org.springframework.http.ResponseEntity;
+
 import java.util.function.Function;
 
 public abstract class BaseController {
@@ -10,4 +13,11 @@ public abstract class BaseController {
         };
     }
 
+    protected ResponseEntity<APIResponse> APIOk(String message) {
+        return ResponseEntity.ok(APIResponse.ok(message));
+    }
+
+    protected ResponseEntity<APIResponse> APIError(String message) {
+        return ResponseEntity.ok(APIResponse.error(message));
+    }
 }
