c38738913ad27549a78c5e00ef5524ffa8b7b0cf
Emerick Poulin <emerick.poulin@omnirobotic.com>
2022-04-02 11:55:41 -0400

    implement image service

---
+++ backend/src/main/java/dev/d34dc0de/lhish/service/ImageService.java
@@ -0,0 +1,26 @@
+package dev.d34dc0de.lhish.service;
+
+import dev.d34dc0de.lhish.model.Image;
+import dev.d34dc0de.lhish.repository.ImageRepository;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class ImageService {
+
+    private final ImageRepository imageRepository;
+
+    public ImageService(ImageRepository imageRepository) {
+        this.imageRepository = imageRepository;
+    }
+
+    public Image insert(Image image) {
+        return imageRepository.save(image);
+    }
+
+    public Optional<Image> findById(String id) {
+        return imageRepository.findById(id);
+    }
+}
