09e5c3f91fa7309945aa7bfa1f7e465c23ccc989
Emerick Poulin <emerick.poulin@omnirobotic.com>
2022-04-02 11:54:56 -0400

    add image model

---
+++ backend/src/main/java/dev/d34dc0de/lhish/model/Image.java
@@ -0,0 +1,20 @@
+package dev.d34dc0de.lhish.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.bson.types.Binary;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Document(collection = "images")
+public class Image {
+    @Id
+    private String id;
+    private Binary data;
+}
