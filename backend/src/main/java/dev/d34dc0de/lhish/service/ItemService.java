4c309026a39885812dde7fccd00f62e4ed24dfc9
Emerick Poulin <emerick.poulin@omnirobotic.com>
2022-04-02 11:55:48 -0400

    use image service

--- backend/src/main/java/dev/d34dc0de/lhish/service/ItemService.java
+++ backend/src/main/java/dev/d34dc0de/lhish/service/ItemService.java
@@ -4,11 +4,10 @@ import dev.d34dc0de.lhish.exceptions.NotFoundException;
 import dev.d34dc0de.lhish.exceptions.NullFieldException;
 import dev.d34dc0de.lhish.model.Item;
 import dev.d34dc0de.lhish.repository.ItemRepository;
-import dev.d34dc0de.lhish.response.APIResponse;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ItemService {
@@ -23,18 +22,22 @@ public class ItemService {
     }
 
     public Item insert(Item item) throws NotFoundException {
-        if(item.getCategoryId() == null) {
-            throw new NullFieldException("Category id cannot be null");
-        }
+//        if(item.getCategoryId() == null) {
+//            throw new NullFieldException("Category id cannot be null");
+//        }
 
-        if (categoryService.findById(item.getCategoryId()).isEmpty()) {
-            throw new NotFoundException("Category", item.getCategoryId());
-        }
+//        if (categoryService.findById(item.getCategoryId()).isEmpty()) {
+//            throw new NotFoundException("Category", item.getCategoryId());
+//        }
 
         return itemRepository.insert(item);
     }
 
     public List<Item> getAll() {
         return itemRepository.findAll();
+    }
+
+    public Optional<Item> findById(String id) {
+        return itemRepository.findById(id);
     }
 }
