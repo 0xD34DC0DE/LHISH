9ca8f4a7a2ef239b16837a32565095836f1c3022
Emerick Poulin <emerick.poulin@omnirobotic.com>
2022-04-03 16:43:47 -0400

    add ItemHistory model

---
+++ backend/src/main/java/dev/d34dc0de/lhish/model/ItemHistory.java
@@ -0,0 +1,23 @@
+package dev.d34dc0de.lhish.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+
+import java.util.List;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Data
+public class ItemHistory {
+    @Id
+    private String id;
+
+    public record ItemHistoryEntry(String accountId, String actionDescription) {
+    }
+
+    private List<ItemHistoryEntry> entries;
+}
