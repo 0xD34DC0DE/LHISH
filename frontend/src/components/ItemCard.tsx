bcb86ecab5b54527c14e8ba4e29189899af39f54
Emerick Poulin <emerick.poulin@omnirobotic.com>
2022-04-03 12:30:01 -0400

    fix description formatting

--- frontend/src/components/ItemCard.tsx
+++ frontend/src/components/ItemCard.tsx
@@ -3,13 +3,13 @@ import UserHistory from "../icons/UserHistoryIcon";
 import NotListedLocationIcon from "@mui/icons-material/NotListedLocation";
 import ClearIcon from "@mui/icons-material/Clear";
 import React from "react";
-import IItem, { Availability } from "../models/ItemView";
+import IItem, {Availability} from "../models/ItemView";
 
 export interface IItemCardComponent {
 }
 
 export const ItemCard: IItemCardComponent & React.FunctionComponent<IItem> =
-    ({id, name, description, imageId, availability}:IItem) => {
+    ({id, name, description, imageId, availability}: IItem) => {
 
         return (
             <Card variant="outlined" sx={{borderRadius: 2, borderWidth: 2, borderColor: "#a1a1a1"}}>
@@ -21,15 +21,10 @@ export const ItemCard: IItemCardComponent & React.FunctionComponent<IItem> =
                 <CardContent>
                     <Typography variant="h5">{name}</Typography>
                     {/*<Typography sx={{fontSize: 14}} color="text.secondary" gutterBottom>*/}
-                    {description} {/*TODO Make availability component to render icon*/}
+                    {/*TODO Make availability component to render icon*/}
                     <Typography sx={{fontSize: 14}}>{Availability[availability]}</Typography>
-                    {
-                        [...Array(Math.round((Math.random() * 10) + 1.0))]
-                            .map((e, i) => {
-                                return <Typography key={i} sx={{fontSize: 14}} color="text.secondary"
-                                                   gutterBottom>{id}</Typography>
-                            })
-                    }
+                    <Typography sx={{fontSize: 14}} color="text.secondary"
+                                gutterBottom>{description}</Typography>
                 </CardContent>
                 <CardActions disableSpacing>
                     <IconButton aria-label="View user history">
