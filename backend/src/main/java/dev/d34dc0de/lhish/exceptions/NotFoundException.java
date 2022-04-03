0d475a1c1431285d63171dabb4c2855104775055
Emerick Poulin <emerick.poulin@omnirobotic.com>
2022-04-03 12:28:20 -0400

    tweak exception formatting

--- backend/src/main/java/dev/d34dc0de/lhish/exceptions/NotFoundException.java
+++ backend/src/main/java/dev/d34dc0de/lhish/exceptions/NotFoundException.java
@@ -2,6 +2,6 @@ package dev.d34dc0de.lhish.exceptions;
 
 public class NotFoundException extends APIException {
     public NotFoundException(String message, String objectId) {
-        super(message + ": " + (objectId == null ? "null" : objectId));
+        super(message + ": " + (objectId == null ? "null" : objectId) + " not found");
     }
 }
