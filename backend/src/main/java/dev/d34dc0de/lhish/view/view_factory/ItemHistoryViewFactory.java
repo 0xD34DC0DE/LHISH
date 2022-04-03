24b4ebd9f740c63a8761461de5f9437970efb0ac
Emerick Poulin <emerick.poulin@omnirobotic.com>
2022-04-03 16:44:37 -0400

    implement ItemHistoryView factory

---
+++ backend/src/main/java/dev/d34dc0de/lhish/view/view_factory/ItemHistoryViewFactory.java
@@ -0,0 +1,25 @@
+package dev.d34dc0de.lhish.view.view_factory;
+
+import dev.d34dc0de.lhish.model.Account;
+import dev.d34dc0de.lhish.model.ItemHistory;
+import dev.d34dc0de.lhish.service.AccountService;
+import dev.d34dc0de.lhish.view.ItemHistoryView;
+
+public abstract class ItemHistoryViewFactory {
+    public static ItemHistoryView toItemHistoryView(AccountService accountService, ItemHistory itemHistory) {
+        return ItemHistoryView.builder()
+                .usernames(
+                        itemHistory.getEntries().stream()
+                                .map(ItemHistory.ItemHistoryEntry::accountId)
+                                .map(accountService::getById)
+                                .map(Account::getUsername)
+                                .toList()
+                )
+                .actions(
+                        itemHistory.getEntries().stream()
+                                .map(ItemHistory.ItemHistoryEntry::actionDescription)
+                                .toList()
+                )
+                .build();
+    }
+}
