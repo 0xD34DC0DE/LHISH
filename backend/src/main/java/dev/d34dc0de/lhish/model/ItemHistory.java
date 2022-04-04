7f0d69c47899c75b8919e56eee7d4608ba1e8bc6
Emerick Poulin <emerick.poulin@omnirobotic.com>
2022-04-03 21:53:42 -0400

    remove inner class

--- backend/src/main/java/dev/d34dc0de/lhish/model/ItemHistory.java
+++ backend/src/main/java/dev/d34dc0de/lhish/model/ItemHistory.java
@@ -15,11 +15,5 @@ import java.util.List;
 public class ItemHistory {
     @Id
     private String id;
-
-    public record ItemHistoryEntry(String accountId, String actionDescription) {
-        @Builder
-        public ItemHistoryEntry{}
-    }
-
     private List<ItemHistoryEntry> entries;
 }
