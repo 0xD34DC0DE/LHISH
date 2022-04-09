3b6b977cd3de52a12960f55d45deb886195080a6
Emerick Poulin <emerick.poulin@omnirobotic.com>
2022-04-09 14:16:18 -0400

    add item history view

--- backend/src/main/java/dev/d34dc0de/lhish/view/ItemHistoryView.java
+++ backend/src/main/java/dev/d34dc0de/lhish/view/ItemHistoryView.java
@@ -2,9 +2,11 @@ package dev.d34dc0de.lhish.view;
 
 import lombok.Builder;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
-public record ItemHistoryView(List<String> accountNames, List<String> actions) {
-    @Builder
-    public ItemHistoryView{}
+public record ItemHistoryView(List<String> accountNames, List<String> actions, List<LocalDateTime> timestamps) {
+        @Builder
+        public ItemHistoryView {
+        }
 }
