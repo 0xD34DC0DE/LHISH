08667c9be89acd5fbe4147ea3c5249c4f3966065
Emerick Poulin <emerick.poulin@omnirobotic.com>
2022-04-09 14:18:11 -0400

    integrate item deletion

--- frontend/src/components/ItemCard.tsx
+++ frontend/src/components/ItemCard.tsx
@@ -6,42 +6,52 @@ import React, {useRef} from "react";
 import IItem, {Availability} from "../views/ItemView";
 import {DialogBaseRef} from "./DialogBase";
 import {ItemHistoryDialog} from "./ItemHistoryDialog";
+import {useDeleteEntity} from "../hooks/CardDeleteHook";
 
-export interface IItemCardComponent {
+interface ItemCardProps extends IItem {
+    onDelete: () => void;
 }
 
-export const ItemCard: IItemCardComponent & React.FunctionComponent<IItem> =
-    ({id, name, description, imageId, availability, historyId}: IItem) => {
-        const dialogRef = useRef<DialogBaseRef>(null);
-        return (
-            <>
-                <Card variant="outlined" sx={{borderRadius: 2, borderWidth: 2, borderColor: "#a1a1a1"}}>
-                    <CardMedia
-                        component="img"
-                        height="140"
-                        image={`http://localhost:8080/image/${imageId}`} // TODO replace with getImageUrl for authentication
-                    />
-                    <CardContent>
-                        <Typography variant="h5">{name}</Typography>
-                        {/*<Typography sx={{fontSize: 14}} color="text.secondary" gutterBottom>*/}
-                        {/*TODO Make availability component to render icon*/}
-                        <Typography sx={{fontSize: 14}}>{Availability[availability]}</Typography>
-                        <Typography sx={{fontSize: 14}} color="text.secondary"
-                                    gutterBottom>{description}</Typography>
-                    </CardContent>
-                    <CardActions disableSpacing>
-                        <IconButton aria-label="View user history" onClick={() => dialogRef.current?.openDialog()}>
-                            <UserHistory/>
-                        </IconButton>
-                        <IconButton aria-label="Localize">
-                            <NotListedLocationIcon/>
-                        </IconButton>
-                        <IconButton aria-label="Localize" sx={{marginLeft: "auto"}}>
-                            <ClearIcon/>
-                        </IconButton>
-                    </CardActions>
-                </Card>
-                <ItemHistoryDialog itemHistoryId={historyId} innerRef={dialogRef}/>
-            </>
-        );
-    }
+export const ItemCard = ({id, name, description, imageId, availability, historyId, onDelete}: ItemCardProps) => {
+    const historyDialogRef = useRef<DialogBaseRef>(null);
+    const [getDialogs, openDeleteDialog] = useDeleteEntity(
+        "category",
+        () => `http://localhost:8080/item/${id}`,
+        onDelete
+    );
+
+    return (
+        <>
+            <Card variant="outlined" sx={{borderRadius: 2, borderWidth: 2, borderColor: "#a1a1a1"}}>
+                <CardMedia
+                    component="img"
+                    height="140"
+                    image={`http://localhost:8080/image/${imageId}`} // TODO replace with getImageUrl for authentication
+                />
+                <CardContent>
+                    <Typography variant="h5">{name}</Typography>
+                    {/*<Typography sx={{fontSize: 14}} color="text.secondary" gutterBottom>*/}
+                    {/*TODO Make availability component to render icon*/}
+                    <Typography sx={{fontSize: 14}}>{Availability[availability]}</Typography>
+                    <Typography sx={{fontSize: 14}} color="text.secondary"
+                                gutterBottom>{description}</Typography>
+                </CardContent>
+                <CardActions disableSpacing>
+                    <IconButton aria-label="View user history"
+                                onClick={() => historyDialogRef.current?.openDialog()}>
+                        <UserHistory/>
+                    </IconButton>
+                    <IconButton aria-label="Localize">
+                        <NotListedLocationIcon/>
+                    </IconButton>
+                    <IconButton aria-label="Localize" sx={{marginLeft: "auto"}}
+                                onClick={openDeleteDialog}>
+                        <ClearIcon/>
+                    </IconButton>
+                </CardActions>
+            </Card>
+            <ItemHistoryDialog innerRef={historyDialogRef} itemHistoryId={historyId}/>
+            {getDialogs()}
+        </>
+    );
+}
