b301c946dd48ee7f7b9b3d71eaba0c3669e02bf1
Emerick Poulin <emerick.poulin@omnirobotic.com>
2022-04-11 20:19:46 -0400

    add factory method for value field view list

--- backend/src/main/java/dev/d34dc0de/lhish/view/view_factory/ValueFieldViewFactory.java
+++ backend/src/main/java/dev/d34dc0de/lhish/view/view_factory/ValueFieldViewFactory.java
@@ -4,6 +4,7 @@ import dev.d34dc0de.lhish.model.Template;
 import dev.d34dc0de.lhish.model.ViewFields.ValueField;
 import dev.d34dc0de.lhish.service.TemplateService;
 import dev.d34dc0de.lhish.view.ValueFieldView;
+import dev.d34dc0de.lhish.view.ValueFieldViewList;
 
 import java.util.List;
 
@@ -13,13 +14,6 @@ public abstract class ValueFieldViewFactory {
                 .name(valueField.getName())
                 .type(valueField.getType())
                 .type(valueField.getType())
-                .layout(valueField.getFieldLayout())
                 .build();
-    }
-
-    static public List<ValueFieldView> toValueFieldViewList(TemplateService templateService, Template template) {
-        return templateService.getValueFields(template).stream()
-                .map(ValueFieldViewFactory::toValueFieldView)
-                .toList();
     }
 }
