418c001abed0b03c183cf27ddd7cd287b15c3c8f
Emerick Poulin <emerick.poulin@omnirobotic.com>
2022-04-12 20:24:19 -0400

    add field mapping to card

--- frontend/src/components/ItemCard.tsx
+++ frontend/src/components/ItemCard.tsx
@@ -7,12 +7,26 @@ import IItem, {Availability} from "../views/ItemView";
 import {DialogBaseRef} from "./DialogBase";
 import {ItemHistoryDialog} from "./ItemHistoryDialog";
 import {useDeleteEntity} from "../hooks/CardDeleteHook";
+import {Field} from "../card_field_components/Fields";
+import {ValueFieldFactory} from "../card_field_components/card_field/base/ValueFieldFactory";
 
 interface ItemCardProps extends IItem {
     onDelete: () => void;
+    imageUrl?: string | null;
+    fields?: (Field | null)[];
 }
 
-export const ItemCard = ({id, name, description, imageId, availability, historyId, onDelete}: ItemCardProps) => {
+export const ItemCard = ({
+                             id = null,
+                             name,
+                             description,
+                             imageId = null,
+                             availability = null,
+                             historyId = null,
+                             onDelete,
+                             imageUrl = null,
+                             fields = []
+                         }: ItemCardProps) => {
     const historyDialogRef = useRef<DialogBaseRef>(null);
     const [getDialogs, openDeleteDialog] = useDeleteEntity(
         "category",
@@ -20,37 +34,60 @@ export const ItemCard = ({id, name, description, imageId, availability, historyI
         onDelete
     );
 
+    const deleteButtonClicked = () => {
+        if (id) {
+            openDeleteDialog();
+        }
+    };
+
+    const onHistoryButtonClicked = () => {
+        if (historyId) {
+            historyDialogRef.current?.openDialog();
+        }
+    };
+
+    const getImageUrl = () => {
+        if (imageUrl) {
+            return imageUrl;
+        }
+        if (imageId === "") {
+            return "";
+        }
+        return `http://localhost:8080/image/${imageId}`;
+    }
+
     return (
         <>
             <Card variant="outlined" sx={{borderRadius: 2, borderWidth: 2, borderColor: "#a1a1a1"}}>
                 <CardMedia
                     component="img"
                     height="140"
-                    image={imageId == "" ? "" : `http://localhost:8080/image/${imageId}`}
+                    image={getImageUrl()}
                 />
                 <CardContent>
                     <Typography variant="h5">{name}</Typography>
                     {/*<Typography sx={{fontSize: 14}} color="text.secondary" gutterBottom>*/}
                     {/*TODO Make availability component to render icon*/}
-                    <Typography sx={{fontSize: 14}}>{Availability[availability]}</Typography>
+                    {availability && <Typography sx={{fontSize: 14}}>{Availability[availability]}</Typography>}
                     <Typography sx={{fontSize: 14}} color="text.secondary"
                                 gutterBottom>{description}</Typography>
+                    <ValueFieldFactory fields={fields}/>
                 </CardContent>
                 <CardActions disableSpacing>
                     <IconButton aria-label="View user history"
-                                onClick={() => historyDialogRef.current?.openDialog()}>
+                                onClick={onHistoryButtonClicked}>
                         <UserHistory/>
                     </IconButton>
                     <IconButton aria-label="Localize">
                         <NotListedLocationIcon/>
                     </IconButton>
                     <IconButton aria-label="Localize" sx={{marginLeft: "auto"}}
-                                onClick={openDeleteDialog}>
+                                onClick={deleteButtonClicked}>
                         <ClearIcon/>
                     </IconButton>
                 </CardActions>
             </Card>
-            <ItemHistoryDialog innerRef={historyDialogRef} itemHistoryId={historyId}/>
+            <ItemHistoryDialog innerRef={historyDialogRef} itemHistoryId={historyId ?? ""}/>
             {getDialogs()}
         </>
     );
