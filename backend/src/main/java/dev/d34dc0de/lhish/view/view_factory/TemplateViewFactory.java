5723a98729921aa3e2b769c4023a66f12ce813c3
Emerick Poulin <emerick.poulin@omnirobotic.com>
2022-04-13 21:05:40 -0400

    remove dead code

--- backend/src/main/java/dev/d34dc0de/lhish/view/view_factory/TemplateViewFactory.java
+++ backend/src/main/java/dev/d34dc0de/lhish/view/view_factory/TemplateViewFactory.java
@@ -1,18 +1,11 @@
 package dev.d34dc0de.lhish.view.view_factory;
 
 import dev.d34dc0de.lhish.model.Template;
-import dev.d34dc0de.lhish.service.TemplateService;
 import dev.d34dc0de.lhish.view.TemplateIdNamePairListView;
-import dev.d34dc0de.lhish.view.TemplateView;
 
 import java.util.List;
 
 public abstract class TemplateViewFactory {
-    static public TemplateView toTemplateView(TemplateService templateService, Template template) {
-        return TemplateView.builder().build();
-//                .valueFieldViews(ValueFieldViewFactory.toValueFieldViewList(templateService))
-//                .build();
-    }
 
     public static TemplateIdNamePairListView toTemplateIdNamePairListView(List<Template> templates) {
         return TemplateIdNamePairListView.builder()
