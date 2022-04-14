b4f83e73f1856bdd24c51c3bc982784a3d1ac0a4
Emerick Poulin <emerick.poulin@omnirobotic.com>
2022-04-13 20:32:48 -0400

    add template id name pair list view to factory

--- backend/src/main/java/dev/d34dc0de/lhish/view/view_factory/TemplateViewFactory.java
+++ backend/src/main/java/dev/d34dc0de/lhish/view/view_factory/TemplateViewFactory.java
@@ -2,12 +2,22 @@ package dev.d34dc0de.lhish.view.view_factory;
 
 import dev.d34dc0de.lhish.model.Template;
 import dev.d34dc0de.lhish.service.TemplateService;
+import dev.d34dc0de.lhish.view.TemplateIdNamePairListView;
 import dev.d34dc0de.lhish.view.TemplateView;
+
+import java.util.List;
 
 public abstract class TemplateViewFactory {
     static public TemplateView toTemplateView(TemplateService templateService, Template template) {
-        return TemplateView.builder()
-                .valueFieldViews(ValueFieldViewFactory.toValueFieldViewList(templateService, template))
+        return TemplateView.builder().build();
+//                .valueFieldViews(ValueFieldViewFactory.toValueFieldViewList(templateService))
+//                .build();
+    }
+
+    public static TemplateIdNamePairListView toTemplateIdNamePairListView(List<Template> templates) {
+        return TemplateIdNamePairListView.builder()
+                .templateIds(templates.stream().map(Template::getId).toList())
+                .templateNames(templates.stream().map(Template::getName).toList())
                 .build();
     }
 }
