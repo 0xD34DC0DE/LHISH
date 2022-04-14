0c0ef8ed9509e7c80cc9c394c686cf166cbae5d8
Emerick Poulin <emerick.poulin@omnirobotic.com>
2022-04-13 20:31:10 -0400

    remove unnecessary ValueField interface methods

--- backend/src/main/java/dev/d34dc0de/lhish/view/ValueFieldView.java
+++ backend/src/main/java/dev/d34dc0de/lhish/view/ValueFieldView.java
@@ -1,10 +1,9 @@
 package dev.d34dc0de.lhish.view;
 
-import dev.d34dc0de.lhish.model.enums.FieldLayout;
 import dev.d34dc0de.lhish.model.enums.ValueType;
 import lombok.Builder;
 
-public record ValueFieldView(String name, Object value, ValueType type, FieldLayout layout) {
+public record ValueFieldView(String name, Object value, ValueType type) {
     @Builder
     public ValueFieldView {
     }
