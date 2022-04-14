07bbede5529cc13cf7bca8271767ebecd96b1d4c
Emerick Poulin <emerick.poulin@omnirobotic.com>
2022-04-13 20:26:34 -0400

    implement validation

--- frontend/src/card_field_components/form_field/IntegerFieldInput.tsx
+++ frontend/src/card_field_components/form_field/IntegerFieldInput.tsx
@@ -1,45 +1,87 @@
-import React, {forwardRef, useImperativeHandle} from "react";
-import {FormFieldRef} from "./FormFieldRef";
+import React, {forwardRef, useEffect, useImperativeHandle} from "react";
+import {FormFieldRef} from "./base/FormFieldRef";
 import {useInput} from "../../hooks/InputHook";
 import {Stack, TextField} from "@mui/material";
-import {ValueFieldInput} from "./ValueFieldInput";
+import {ValueFieldInput} from "./base/ValueFieldInput";
+import {ValueType} from "../ValueTypes";
+import {useValidation} from "../../hooks/ValidationHook";
+import {Field} from "../Fields";
 
 export interface NumberFieldInputProps extends ValueFieldInput {
 }
 
 export const IntegerFieldInput = forwardRef<FormFieldRef, NumberFieldInputProps>(
-    ({existingName}: NumberFieldInputProps, ref) => {
-    const [name, setName] = useInput();
-    const [value, setValue] = useInput(v => v.replaceAll(".", ""));
+    ({existingName, onFieldChange}: NumberFieldInputProps, ref) => {
+        const [name, setName] = useInput();
+        const [value, setValue] = useInput(v => v.replaceAll(".", ""));
 
-    useImperativeHandle(ref, () => ({
-        getName: () => name,
-        getValue: () => value,
-    }));
+        const [nameValidation, nameErrorProps] = useValidation(() => name === "" ? "Name is required" : null);
+        const [valueValidation, valueErrorProps] = useValidation(
+            () => isNaN(parseInt(value)) ? "Value is required" : null
+        );
 
-    return (
-        <Stack direction={"row"}>
-            <TextField
-                margin="dense"
-                id="name"
-                value={name}
-                label={existingName ?? "Name"}
-                disabled={existingName !== null}
-                type="text"
-                fullWidth
-                onChange={setName}
-                sx={{marginRight: 1}}
-            />
-            <TextField
-                margin="dense"
-                id="value"
-                label="Value"
-                type="number"
-                value={value}
-                fullWidth
-                onChange={setValue}
-                sx={{marginLeft: 1}}
-            />
-        </Stack>
-    );
-});
+        useImperativeHandle(ref, () => ({
+            getField: getField
+        }));
+
+        const isValid = (skipErrorMessage: boolean = false) => {
+            return [
+                nameValidation(skipErrorMessage),
+                valueValidation(skipErrorMessage)
+            ].every(v => v);
+        }
+
+        const getField = (): Field | null => {
+            if (!isValid()) {
+                return null;
+            }
+            return {
+                type: ValueType.INTEGER,
+                name: name,
+                value: parseInt(value)
+            };
+        }
+
+        useImperativeHandle(ref, () => ({
+            getField: getField
+        }));
+
+        useEffect(() => {
+            if(!isValid(true)) {
+                return;
+            }
+            let field = getField();
+            if (field !== null) {
+                onFieldChange(field);
+            }
+        }, [name, value]);
+
+        return (
+            <Stack direction={"row"}>
+                <TextField
+                    margin="dense"
+                    id="name"
+                    value={name}
+                    label={existingName ?? "Name"}
+                    disabled={existingName !== null}
+                    type="text"
+                    fullWidth
+                    onChange={setName}
+                    {...nameErrorProps}
+                    sx={{marginRight: 1}}
+                />
+                <TextField
+                    margin="dense"
+                    id="value"
+                    label="Value"
+                    type="number"
+                    value={value}
+                    fullWidth
+                    onChange={setValue}
+                    {...valueErrorProps}
+                    sx={{marginLeft: 1}}
+                />
+            </Stack>
+        );
+    }
+);
