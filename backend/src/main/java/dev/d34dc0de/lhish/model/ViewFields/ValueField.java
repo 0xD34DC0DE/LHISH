719d10e6134392a89800d9970195bb2636b5734d
Emerick Poulin <emerick.poulin@omnirobotic.com>
2022-04-13 20:25:58 -0400

    move value field file

--- backend/src/main/java/dev/d34dc0de/lhish/model/ViewFields/ValueField.java
+++ backend/src/main/java/dev/d34dc0de/lhish/model/ViewFields/ValueField.java
@@ -1,23 +1,19 @@
 package dev.d34dc0de.lhish.model.ViewFields;
 
-import dev.d34dc0de.lhish.model.enums.FieldLayout;
-import dev.d34dc0de.lhish.model.enums.FieldType;
 import dev.d34dc0de.lhish.model.enums.ValueType;
+import lombok.Builder;
 import lombok.Data;
 
 @Data
-public abstract class ValueField {
+public class ValueField {
     private String name;
     private Object value;
     private ValueType type;
 
+    @Builder
     public ValueField(String name, Object value, ValueType type) {
         this.name = name;
         this.value = value;
         this.type = type;
     }
-
-    public abstract FieldType getFieldType();
-
-    public abstract FieldLayout getFieldLayout();
 };
