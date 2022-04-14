de367e4ed84b9bccb8f924ffa730bf0892f692b8
Emerick Poulin <emerick.poulin@omnirobotic.com>
2022-04-13 20:28:55 -0400

    make workaround for template instantiation

--- backend/src/main/java/dev/d34dc0de/lhish/model/Template.java
+++ backend/src/main/java/dev/d34dc0de/lhish/model/Template.java
@@ -1,5 +1,6 @@
 package dev.d34dc0de.lhish.model;
 
+import dev.d34dc0de.lhish.model.ViewFields.ValueField;
 import lombok.Builder;
 import lombok.Data;
 import org.springframework.data.annotation.Id;
@@ -10,13 +11,17 @@ import java.util.List;
 public class Template {
     @Id
     private String id;
+    private String name;
     private boolean isCategoryTemplate;
-    List<String> valueFieldsIds;
+    private Boolean isInstance;
+    List<ValueField> valueFields;
 
     @Builder
-    public Template(String id, List<String> valueFieldsIds) {
+    public Template(String id, String name, Boolean isInstance, List<ValueField> valueFields) {
         this.id = id;
+        this.name = name;
         this.isCategoryTemplate = false; // Not implemented yet
-        this.valueFieldsIds = valueFieldsIds;
+        this.isInstance = isInstance;
+        this.valueFields = valueFields;
     }
 }
