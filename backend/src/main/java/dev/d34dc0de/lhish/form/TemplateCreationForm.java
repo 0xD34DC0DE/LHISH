520f6e6da96837fdc4e6a1cb1ecf6eb159f6135e
Emerick Poulin <emerick.poulin@omnirobotic.com>
2022-04-21 22:06:31 -0400

    move model

--- backend/src/main/java/dev/d34dc0de/lhish/form/TemplateCreationForm.java
+++ backend/src/main/java/dev/d34dc0de/lhish/form/TemplateCreationForm.java
@@ -1,6 +1,6 @@
 package dev.d34dc0de.lhish.form;
 
-import dev.d34dc0de.lhish.model.ViewFields.ValueField;
+import dev.d34dc0de.lhish.model.ValueField;
 import lombok.Builder;
 
 import java.util.List;
