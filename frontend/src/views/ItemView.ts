3e7b9e41c55160a4b63c4c0ecab53e00a5eeefd1
Emerick Poulin <emerick.poulin@omnirobotic.com>
2022-04-21 22:03:21 -0400

    add ItemDto to handle layout change

--- frontend/src/views/ItemView.ts
+++ frontend/src/views/ItemView.ts
@@ -1,3 +1,5 @@
+import {Field} from "../card_field_components/Fields";
+import {ValueType} from "../card_field_components/ValueTypes";
 
 export enum Availability {
     Available,
@@ -13,4 +15,16 @@ export default interface IItem {
     imageId?: string | null,
     availability?: Availability | null,
     historyId?: string | null,
+    fields?: (Field | null)[],
+}
+
+// Required because of the way objects are stored in the backend
+// the field "value" needs to be destructured into a field to become an IItem
+export interface ItemDTO {
+    id?: string | null,
+    name: string | null,
+    description: string | null,
+    imageId?: string | null,
+    historyId?: string | null,
+    fields: {name: string, type: ValueType, value: (Field | null)}[]
 }
