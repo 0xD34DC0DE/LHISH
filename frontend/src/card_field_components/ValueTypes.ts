fb6886c1a82e87d3d8f5ec85d613ec21af571253
Emerick Poulin <emerick.poulin@omnirobotic.com>
2022-04-21 21:59:15 -0400

    workaround for enum values from JSON

--- frontend/src/card_field_components/ValueTypes.ts
+++ frontend/src/card_field_components/ValueTypes.ts
@@ -1,3 +1,4 @@
+
 export enum ValueType {
     INTEGER = 'INTEGER',
     FLOAT = 'FLOAT',
@@ -7,3 +8,25 @@ export enum ValueType {
     FILE = 'FILE',
     IMAGE = 'IMAGE',
 }
+
+// workaround because json doesn't convert correctly to enum
+export const valueTypeFromString = (valueType: string): ValueType => {
+    switch (valueType) {
+        case 'INTEGER':
+            return ValueType.INTEGER;
+        case 'FLOAT':
+            return ValueType.FLOAT;
+        case 'STRING':
+            return ValueType.STRING;
+        case 'INTEGER_SYMBOL':
+            return ValueType.INTEGER_SYMBOL;
+        case 'FLOAT_SYMBOL':
+            return ValueType.FLOAT_SYMBOL;
+        case 'FILE':
+            return ValueType.FILE;
+        case 'IMAGE':
+            return ValueType.IMAGE;
+        default:
+            return ValueType.STRING;
+    }
+};
